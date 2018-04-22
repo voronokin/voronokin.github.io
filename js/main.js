df82f64394de209fa1e000217bb514d9545f431a
Alexander Nikonorov <37952794+voronokin@users.noreply.github.com>
2018-04-22 21:26:25 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,128 @@
+function playA() {
+  var audioA = document.getElementById("audioA");
+  audioA.play();
+}
+
+function playB() {
+  var audioB = document.getElementById("audioB");
+  audioB.play();
+}
+
+function playC() {
+  var audioC = document.getElementById("audioC");
+  audioC.play();
+}
+
+function playD() {
+  var audioD = document.getElementById("audioD");
+  audioD.play();
+}
+
+function playE() {
+  var audioE = document.getElementById("audioE");
+  audioE.play();
+}
+
+function playF() {
+  var audioF = document.getElementById("audioF");
+  audioF.play();
+}
+function playG() {
+  var audioG = document.getElementById("audioG");
+  audioG.play();
+}
+
+function playH() {
+  var audioH = document.getElementById("audioH");
+  audioH.play();
+}
+
+function playI() {
+  var audioI = document.getElementById("audioI");
+  audioI.play();
+}
+
+function playJ() {
+  var audioJ = document.getElementById("audioJ");
+  audioJ.play();
+}
+
+function playK() {
+  var audioK = document.getElementById("audioK");
+  audioK.play();
+}
+
+function playL() {
+  var audioL = document.getElementById("audioL");
+  audioL.play();
+}
+
+function playM() {
+  var audioM = document.getElementById("audioM");
+  audioM.play();
+}
+
+function playN() {
+  var audioN = document.getElementById("audioN");
+  audioN.play();
+}
+
+function playO() {
+  var audioO = document.getElementById("audioO");
+  audioO.play();
+}
+
+function playP() {
+  var audioP = document.getElementById("audioP");
+  audioP.play();
+}
+
+function playQ() {
+  var audioQ = document.getElementById("audioQ");
+  audioQ.play();
+}
+
+function playR() {
+  var audioR = document.getElementById("audioR");
+  audioR.play();
+}
+
+function playS() {
+  var audioS = document.getElementById("audioS");
+  audioS.play();
+}
+
+function playT() {
+  var audioT = document.getElementById("audioT");
+  audioT.play();
+}
+
+function playU() {
+  var audioU = document.getElementById("audioU");
+  audioU.play();
+}
+
+function playV() {
+  var audioV = document.getElementById("audioV");
+  audioV.play();
+}
+
+function playW() {
+  var audioW = document.getElementById("audioW");
+  audioW.play();
+}
+
+function playX() {
+  var audioX = document.getElementById("audioX");
+  audioX.play();
+}
+
+function playY() {
+  var audioY = document.getElementById("audioY");
+  audioY.play();
+}
+
+function playZ() {
+  var audioZ = document.getElementById("audioZ");
+  audioZ.play();
+}
